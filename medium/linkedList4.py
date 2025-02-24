f1963521daeb05829676ef71a0771bd9ffb7e16f
junyi.wang <junyi.wang.007@gmail.com>
2025-02-24 11:40:15 -0700

    update

--- medium/linkedList4.py
+++ medium/linkedList4.py
@@ -89,3 +89,18 @@ class CopyRandomList:
 
         return doppel[head]
 
+
+class AddTwoNumbers:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        dummy = n = ListNode()
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            s = v1 + v2 + carry
+            carry = s // 10
+            n.next = ListNode(s % 10)
+            n = n.next
+            l1, l2 = l1.next if l1 else None, l2.next if l2 else None
+        return dummy.next
+
