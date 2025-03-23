e4447a5b2636614fbb9ad389fbb267b72feaf2c1
junyi.wang <junyi.wang.007@gmail.com>
2025-03-23 09:20:10 -0600

    update

--- medium/greedy.py
+++ medium/greedy.py
@@ -39,3 +39,17 @@ class JumpGame2:
             rs += 1
         return rs
 
+
+class GasStation:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total, rs = 0, 0
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            if total < 0:
+                total = 0
+                rs = i + 1
+        return rs
+
