2b118870c68a0088d526451a2686812dd681b0b4
junyi.wang <junyi.wang.007@gmail.com>
2025-04-11 13:21:26 -0600

    update

--- medium/oneDDP.py
+++ medium/oneDDP.py
@@ -15,3 +15,18 @@ class HouseRobber:
 
         return recurse(0)
 
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.robLine(nums[1:]), self.robLine(nums[:-1]))
+
+    def robLine(self, nums: List[int]) -> int:
+        cache = [-1] * len(nums)
+        def recurse(i: int) -> int:
+            if i >= len(nums):
+                return 0
+            if cache[i] == -1:
+                cache[i] = max(nums[i] + recurse(i+2), recurse(i+1))
+            return cache[i]
+        return recurse(0)
+
