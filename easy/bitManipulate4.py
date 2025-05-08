225fb366c8821d98dc785b4513352a88fdba65d2
junyi.wang <junyi.wang.007@gmail.com>
2025-05-07 18:49:16 -0600

    update

--- easy/bitManipulate4.py
+++ easy/bitManipulate4.py
@@ -17,3 +17,18 @@ class SingleNumber:
             rs ^= n
         return rs
 
+class ReverseBits:
+    def reverseBits(self, n: int) -> int:
+        rs = 0
+        for i in range(32):
+            rs |= ((n >> i) & 1) << (31 - i)
+        return rs
+
+
+class CountBits:
+    def countBits(self, n: int) -> List[int]:
+        rs = [0]
+        for i in range(1, n+1):
+            rs.append(rs[i >> 1] + (i & 1))
+        return rs
+
