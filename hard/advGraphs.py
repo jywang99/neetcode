67aa4836b00b2371234623970d4a9d7fb0a83481
junyi.wang <junyi.wang.007@gmail.com>
2025-05-27 20:58:00 -0600

    update

--- hard/advGraphs.py
+++ hard/advGraphs.py
@@ -40,4 +40,41 @@ class SwimInWater:
                 heapq.heappush(hp, (max(t, grid[nr][nc]), nr, nc))
 
         return -1
-        
+
+
+class AlienDictionary:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+            ml = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:ml] == w2[:ml]:
+                return ""
+            for j in range(ml):
+                c1, c2 = w1[j], w2[j]
+                if c1 != c2:
+                    adj[c1].add(c2)
+                    break
+
+        visit = {}
+        rs = []
+        def recurse(c: str) -> bool:
+            if c in visit:
+                return visit[c]
+
+            visit[c] = True
+            for nc in adj[c]:
+                if recurse(nc):
+                    return True
+            visit[c] = False
+
+            rs.append(c)
+            return False
+
+        for c in adj:
+            if recurse(c):
+                return ""
+
+        rs.reverse()
+        return "".join(rs)
+
--- hard/interval3.py
+++ hard/interval3.py
@@ -1,6 +1,23 @@
+import heapq
 from typing import List
 
 
 class MinInterval:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        
+        intervals.sort()
+        rs = {}
+
+        hp = []
+        i = 0
+        for q in sorted(queries):
+            while i < len(intervals) and intervals[i][0] <= q:
+                l, r = intervals[i]
+                heapq.heappush(hp, (r - l + 1, r))
+                i += 1
+
+            while hp and hp[0][1] < q:
+                heapq.heappop(hp)
+            rs[q] = hp[0][0] if hp else -1
+
+        return [rs[q] for q in queries]
+
